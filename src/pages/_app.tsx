48decf8b1e0d7901311e85218636f45f33b1a6ec
Fandi Agus Riyanto <fandiagusriyanto@Fandis-MacBook-Pro.local>
2022-11-08 11:18:16 +0700

    add and config eslint, prettier, linstaged :sparkles:

---
+++ src/pages/_app.tsx
@@ -0,0 +1,29 @@
+import type {AppProps} from 'next/app';
+import 'antd/dist/antd.css';
+import Head from 'next/head';
+import {createGlobalStyle} from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    width: 100vW;
+    height: 100vH;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    overflow-y: hidden;
+    overflow-x: hidden;
+    background: white;
+  }
+`;
+
+export default function App({Component, pageProps}: AppProps) {
+	return (
+		<>
+			<GlobalStyle />
+			<Head>
+				<title>Boilerplate NextJs</title>
+			</Head>
+			<Component {...pageProps} />
+		</>
+	);
+}
