858fdd9b3bd8e8cfb901a52bac3f586b65bbcca2
Fandi Agus Riyanto <fandiagusriyanto@Fandis-MacBook-Pro.local>
2022-11-08 10:46:36 +0700

    add ant design and styled-components :sparkles:

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,29 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import "antd/dist/antd.css";
+import { createGlobalStyle } from "styled-components";
+import Head from "next/head";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    width: 100vW;
+    height: 100vH;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    overflow-y: hidden;
+    overflow-x: hidden;
+    background: white;
+  }
+`;
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <GlobalStyle />
+      <Head>
+        <title>Boilerplate NextJs</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
